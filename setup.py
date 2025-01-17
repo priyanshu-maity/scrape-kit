1f0adb25ec801f1fc24abdd243f1b09d21240b47
Priyanshu Maity <priyanshu.maity2006@gmail.com>
2025-01-17 06:40:15 +0530

    Minor bug fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='scrape-kit',
-    version='1.0.0',
+    version='1.0.1',
     author='Priyanshu Maity',
     author_email='priyanshu.maity2006@gmail.com',
     description='A toolkit comprising multiple tools to help with web scraping.',
