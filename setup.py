0d46e2787e79cb22800b66c87ed15b2269fb3d1e
Priyanshu Maity <priyanshu.maity2006@gmail.com>
2025-11-19 16:54:07 +0530

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='scrape-kit',
-    version='1.0.1',
+    version='1.0.0',
     author='Priyanshu Maity',
     author_email='priyanshu.maity2006@gmail.com',
     description='A toolkit comprising multiple tools to help with web scraping.',
@@ -15,11 +15,9 @@ setup(
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.10',
     install_requires=['beautifulsoup4', 'tzlocal'],
     include_package_data=True,
-    license='MIT'
 )
