06953ffe7c9e5bec619a9980b018d639135dce7b
Priyanshu Maity <priyanshu.maity2006@gmail.com>
2025-12-02 18:47:49 +0530

    refactor: rename utils.py to auth.py

--- scrapekit/auth.py
+++ scrapekit/auth.py
@@ -1,25 +1,9 @@
 from datetime import datetime
-from tzlocal import get_localzone
 import time
 
 import imaplib
 import email
 from bs4 import BeautifulSoup
-
-
-def convert_utc_to_local(utc_time: datetime) -> datetime:
-    """
-    Converts a UTC datetime object to the user's local timezone.
-
-    Args:
-        utc_time (datetime): The UTC datetime object to be converted.
-
-    Returns:
-        datetime: The converted datetime object in the user's local timezone.
-    """
-    local_timezone = get_localzone()
-    local_time = utc_time.astimezone(local_timezone)
-    return local_time
 
 
 def get_magic_link(
